86d6b1dd43af21a9d3a65583ad855aab6f94e43a
powerwook <powerwook@naver.com>
2022-08-08 07:16:48 +0900

    asd

--- week01/큰수만들기_김현욱.py
+++ week01/큰수만들기_김현욱.py
@@ -1,3 +1,22 @@
-"""
-풀이 작성
-"""
+def solution(number, k):
+    answer = []
+    remove_num = k
+    for s in number:
+        if len(answer)==0:
+            answer.append(s)
+        elif answer[-1] < s and k>0:
+            answer.pop()
+            k -= 1
+            answer.append(s)
+            while len(answer) > 1:
+                if answer[-2] < s and k>0:
+                    answer.pop(-2)
+                    k -= 1
+                else:
+                    break      
+        else:
+            answer.append(s)
+    answer = "".join(answer)
+    if len(answer) != len(number) - remove_num:
+        answer = answer[:-remove_num]
+    return answer
